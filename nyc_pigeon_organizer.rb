f4740e118d21ef1b0eb35bbc7249bc3cbe04d0bb
Enrique Valencia <enrique.valencia@yale.edu>
2020-05-22 02:03:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list ={}
-  data.each do |outer, middle, inner|
+  data.each do |outer, middle|
+    data[outer] do |middle, names|
+      data[middle] do |names|
+        
     
   
 end
